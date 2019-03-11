ce6645ee4cae94983cbb8b34d84e9710d67a312f
fehersanyi-bitrise <sandor.feher@bitrise.io>
2019-03-11 19:27:07 +0100

    set up empty functions on the planning

--- star_gate/star_gate.go
+++ star_gate/star_gate.go
@@ -1,1 +1,27 @@
 package stargate
+
+//Jump will cd into a given directory
+func Jump() {}
+
+//CheckPoint will mark a directory
+func CheckPoint() {}
+
+/*
+this will make a .map file
+that will hold the checkpoints with a
+name=pwd fomat
+*/
+func createMap() {}
+
+/*
+this shall read the .map file and return a path
+based on the input
+*/
+func readMap() {}
+
+/*
+this will write the given path to the .bash_profile
+it is important to log out that after this you need to
+source ~/.bash_profile !!!!
+*/
+func recordLocation() {}
